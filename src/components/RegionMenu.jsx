027126f857ec07e2ec3619b92df64bcd04a05287
DORMODO <ymb01072005@gmail.com>
2025-06-09 19:12:46 +0300

    Add toggle logic for dark/light theme

--- src/components/RegionMenu.jsx
+++ src/components/RegionMenu.jsx
@@ -1,22 +1,27 @@
 import Select from "react-select";
 
 const options = [
-  { value: "chocolate", label: "Chocolate" },
-  { value: "strawberry", label: "Strawberry" },
-  { value: "vanilla", label: "Vanilla" },
+  { value: "africa", label: "Africa" },
+  { value: "americas", label: "Americas" },
+  { value: "asia", label: "Asia" },
+  { value: "europe", label: "Europe" },
+  { value: "oceania", label: "Oceania" },
 ];
 
 export const RegionMenu = () => {
   return (
     <Select
+      placeholder="Filter by Region"
       classNames={{
-        input: () => "dark:!text-gray-100",
-        singleValue: () => "dark:text-gray-100",
+        input: () => "!text-gray-800 dark:!text-gray-100",
+        singleValue: () => "!text-gray-800 dark:!text-gray-100",
+        placeholder: () => "!text-gray-800 dark:!text-gray-100",
         control: () =>
-          "flex h-12 items-center justify-between gap-12 rounded-md !border-none pl-4 pr-2 shadow",
+          "flex h-12 items-center justify-between gap-12 rounded-md !border-none pl-4 pr-2 !bg-white dark:!bg-gray-800 shadow-md",
         indicatorsContainer: () => "hidden",
-        option: () => "hover:!text-gray-800",
-        menu: () => "bg-gray-100 dark:bg-gray-800 dark:text-gray-100" ,
+        option: () =>
+          "!text-gray-800 dark:!text-gray-100 hover:!bg-gray-100 dark:hover:!bg-gray-700 cursor-pointer",
+        menu: () => "!bg-white dark:!bg-gray-800 shadow-md",
       }}
       options={options}
     />
