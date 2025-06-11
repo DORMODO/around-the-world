bad1a755d56e5b0bf54fcfdab5bdd1ea6aedf0f8
DORMODO <ymb01072005@gmail.com>
2025-06-12 00:01:28 +0300

    Fix loading state and handle not found scenario in CountryList component

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -1,7 +1,12 @@
+import { NotFound } from "./common/NotFound";
 import { CountryCard } from "./CountryCard";
 import { Loading } from "./Loading";
 
 export const CountryList = ({ data }) => {
+  if (!data) {
+    return <Loading />;
+  }
+
   return (
     <div className="mt-8 grid grid-cols-1 justify-between gap-x-[70px] gap-y-12 md:mt-12 md:grid-cols-[repeat(2,minmax(0,_auto))] lg:grid-cols-[repeat(4,minmax(0,_auto))] lg:gap-y-[70px]">
       {data && data.length ? (
@@ -17,7 +22,7 @@ export const CountryList = ({ data }) => {
           />
         ))
       ) : (
-        <Loading />
+        <NotFound />
       )}
     </div>
   );
